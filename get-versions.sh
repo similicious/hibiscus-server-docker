816012f8e27d6b87f148fcaf7690f6d6ad539656
similicious <mail@similicious.de>
2025-02-17 18:23:23 +0100

    chore: test version.json generation

--- get-versions.sh
+++ get-versions.sh
@@ -4,4 +4,4 @@ grep -oP 'Version \d\.\d{2}\.\d{2}' | \
 sed 's/Version //' | \
 jq -nR '[inputs | {version: ., is_latest: false}] 
         | if length > 0 then .[0].is_latest = true else . end 
-        | .[:3]' > versions.json
+        | .[:4]' > versions.json
