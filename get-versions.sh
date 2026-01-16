c27ffbf69993e11a1d4b4a900e4635e2556426f2
similicious <mail@similicious.de>
2026-01-16 14:22:17 +0100

    fix: get version script doesnt catch single digit patch and minor versions

--- get-versions.sh
+++ get-versions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sL "https://www.willuhn.de/products/hibiscus-server/changelog.php" | \
-grep -oP 'Version \d\.\d{2}\.\d{2}' | \
+grep -oP 'Version \d\.\d{1,2}\.\d{1,2}' | \
 sed 's/Version //' | \
 jq -nR '[inputs | {version: ., is_latest: false}] 
         | if length > 0 then .[0].is_latest = true else . end 
